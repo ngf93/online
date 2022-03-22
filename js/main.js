22a2fd8bebc3f67b0a56b0f1bf2bdd38f9b3302a
DimaS <95845156+arthurad737@users.noreply.github.com>
2022-03-22 19:04:16 +0300

    completed

--- js/main.js
+++ js/main.js
@@ -9,6 +9,65 @@ function changeState(btn) {
     }
 }
 
+/*************** 
+CUSTOM SELECT
+****************/
+window.onload = function() {
+  let selectElement = document.querySelectorAll('.custom-select');
+  let arrSelects = Array.from(selectElement);
+  arrSelects.forEach(function(item, i, arr) {
+      const btn = item.querySelector('button');
+      const options = item.querySelector('.cs-options');
+      let inputs = item.querySelectorAll('input');
+      // let name = inputs[0].name;
+
+      // let cur_inp; //выбранный input radio
+      // for(let i = 0; i < inputs.length; i++){
+      //   if(inputs[i].checked){
+      //     cur_inp = inputs[i];
+      //   }
+      // }
+
+      // if(cur_inp) {
+      //   let label_content = cur_inp.nextElementSibling.innerHTML; //содержимое label следующего после выбранного input
+      //   btn.innerHTML = label_content;
+      // } else {
+      //   btn.innerHTML = "Не выбрано";
+      // }
+
+      // открываем/закрываем окно навигации, добаляя/удаляя активный класс
+      const toggleMenu = function() {
+        options.classList.toggle('opened');
+      }
+
+      btn.addEventListener('click', () => { // при клике на кнопку
+        toggleMenu();
+      });
+      
+      //скрываем опции при клике вне селекта
+      document.addEventListener('click', function(e) {
+        const target = e.target;
+        const current_sel = target == item || item.contains(target);
+        const sel_is_opened = options.classList.contains('opened');
+        if (!current_sel && sel_is_opened) {
+          toggleMenu();
+        }
+      });
+
+      let arrRadio = Array.from(inputs);
+      arrRadio.forEach(function(item, i, arr) {
+        item.addEventListener('change', (event) => {
+          changeValue(item);
+        });
+      });
+  
+      function changeValue(el){
+        btn.innerHTML = el.value;
+        toggleMenu();
+      }
+  });
+}
+
 
 const anchors = document.querySelectorAll('a.scroll-to')
 
@@ -24,3 +83,27 @@ for (let anchor of anchors) {
     })
   })
 }
+
+const swiper = new Swiper('.swiper', {
+  
+  direction: 'horizontal',
+  slidesPerView: 1,
+  spaceBetween: 20,
+  grabCursor: true,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      spaceBetween: 30,
+    },
+    992: {
+      direction: 'vertical',
+      pagination: false
+    }
+  }
+});
