8d3c5e83272daf0fdcf9e9e9544bf1e98586aaea
DimaS <95845156+arthurad737@users.noreply.github.com>
2022-03-24 13:12:12 +0300

    css scroll

--- js/main.js
+++ js/main.js
@@ -51,24 +51,6 @@ window.onload = function() {
   });
 }
 
-/* smopth scroll to anchor */
-document.querySelectorAll('a.scroll-to').forEach(link => {
-  link.addEventListener('click', function(e) {
-      e.preventDefault();
-      let href = this.getAttribute('href').substring(1);
-      const scrollTarget = document.getElementById(href);
-      // const topOffset = document.querySelector('.scrollto').offsetHeight;
-      const topOffset = 0; // если не нужен отступ сверху 
-      const elementPosition = scrollTarget.getBoundingClientRect().top;
-      const offsetPosition = elementPosition - topOffset;
-      window.scrollBy({
-          top: offsetPosition,
-          behavior: 'smooth'
-      });
-  });
-});
-
-
 const swiper = new Swiper('.swiper', {
   direction: 'horizontal',
   slidesPerView: 1,
